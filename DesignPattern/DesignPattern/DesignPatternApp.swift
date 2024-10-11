f4e260c69b9802047821e6406311f45fce54a5ed
akira furuya <akira@yamato.local>
2024-10-11 16:23:51 +0900

    init commit

---
+++ DesignPattern/DesignPattern/DesignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternApp.swift
+//  DesignPattern
+//
+//  Created by AKIRA on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
